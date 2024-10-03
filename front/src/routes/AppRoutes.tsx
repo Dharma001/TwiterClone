437e26bff0ac2902f4582d329d29130a6c747311
Dharma Bahadur Budhathoki <117076233+Dharma001@users.noreply.github.com>
2024-10-03 20:44:03 +0545

    Feat/front0.1 (#3)
    * feat:added tailwind css
    
    * fixes

---
+++ front/src/routes/AppRoutes.tsx
@@ -0,0 +1,18 @@
+// src/routes/AppRoutes.tsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Auth/Login';
+import Register from '../pages/Auth/Register';
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
