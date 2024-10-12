9dab5040f4316e3c92c17f30393331c24671ebf3
Dharma Bahadur Budhathoki <117076233+Dharma001@users.noreply.github.com>
2024-10-12 12:14:23 +0545

    Feat/auth0.7 (#25)
    * feat:added tailwind css
    
    * fixed the conficts
    
    * fixed the conficts
    
    * fixed the conficts
    
    * feat:Added x .com auth page
    
    * x.com style front end complete
    
    * added google auth
    
    * feat:Added websockts and added register and otp
    
    * feat:Added websockts and added register and otp
    
    * feat:Added register , verify otp and create password
    
    * regiser otp and password styles fixed
    
    * feat:Added login
    
    * feat: modified routes
    
    * made some style chnages

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,19 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home: React.FC = () => {
+  const [isForYouPostActive , setForYouPostInActive] = useState(false);
+  const [isFollowingPostActive , setFollowingPostInActive] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
+  const forYouPost = () => {
+    setForYouPostInActive(true)
+    setFollowingPostInActive(false)
+  }
+
+  const following = () => {
+    setFollowingPostInActive(true)
+    setForYouPostInActive(false)
+  }
   return (
     <>
-    <div className="">
-        <nav>
-          <ul>
-            <li>
+    <div className="flex gap-5 h-full">
+      <div className="relative w-[70%] border-r border-gray-500">
+        <nav className='absolute top-0 w-full'>
+          <ul className={`grid grid-cols-2 border-b text-white py-[2px] ${isScrolled ? 'bg-gray-900 bg-opacity-50' : ''}`}>
+            <li onClick={forYouPost} className='flex justify-center items-center hover:bg-gray-900 hover:bg-opacity-90 border-r border-gray-500'>
+              <span className={`p-3 text-md ${isForYouPostActive && !isFollowingPostActive ? 'border-b-[4px] rounded-sm border-blue-500 font-semibold' : 'font-medium'}`}>
               For You
+              </span>
             </li>
-            <li>
-              Following
+            <li onClick={following} className='flex justify-center items-center hover:bg-gray-900 hover:bg-opacity-90'>
+              <span className={`p-3 text-md ${!isForYouPostActive && isFollowingPostActive ? 'border-b-[4px] rounded-sm border-blue-500 font-semibold' : 'font-medium'}`}>
+                Following
+              </span>
             </li>
           </ul>
         </nav>
+        </div>
+        <div className="w-[40%]">
+          <h1>search</h1>
+        </div>
     </div>
     </>
   )
